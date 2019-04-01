cfbd777c84ddcbd5f3a90cb2514bb3eb59d35f70
clyde9 <samueljclyde@gmail.com>
2019-04-01 10:25:43 -0600

    added buildPar function, separated rows and columns

--- main.js
+++ main.js
@@ -1,21 +1,37 @@
-let courseData = null;
-
 function buildHoles() {
-    $(".container").append(`<div class="baseRow"><div class="col">Holes</div></div>`);
+    $(".container").append(`<div class="row holesRow"><div class="col">Holes</div></div>`);
     for (let i = 0; i < 9; i++) {
-        $('.baseRow').append(`<div class="col">${i + 1}</div>`);
+        $('.holesRow').append(`<div class="col">${i + 1}</div>`);
     }
-    $(".baseRow").append(`<div class="col">Out</div>`);
+    $(".holesRow").append(`<div class="col">Out</div>`);
     for (let i = 9; i < 18; i++) {
-        $('.baseRow').append(`<div class="col">${i + 1}</div>`);
+        $('.holesRow').append(`<div class="col">${i + 1}</div>`);
     }
-    $(".baseRow").append(`<div class="col">In</div><div class="col">Total</div>`);
+    $(".holesRow").append(`<div class="col">In</div><div class="col">Total</div>`);
 }
 
+function buildPar(courseData){
+    $(".container").append(`<div class="row parRow"><div class="col">Par</div></div>`);
+    let outPar = 0;
+    for (let j = 0; j < 9; j++) {
+        $(`.parRow`).append(`<div class="col">${courseData.data.holes[j].teeBoxes[0].par}</div>`);
+        outPar += courseData.data.holes[j].teeBoxes[0].par;
+    }
+    $(`.parRow`).append(`<div class="col">${outPar}</div>`);
+    let inPar = 0;
+    for (let j = 9; j < 18; j++) {
+        $(`.parRow`).append(`<div class="col">${courseData.data.holes[j].teeBoxes[0].par}</div>`);
+        inPar += courseData.data.holes[j].teeBoxes[0].par;
+    }
+    $(`.parRow`).append(`<div class="col">${inPar}</div><div class="col">${outPar + inPar}</div>`);
+}
+
+
+
 $.ajax({
-    url: "https://golf-courses-api.herokuapp.com/courses/18300"
+    url: "https://golf-courses-api.herokuapp.com/courses/11819"
 }).done(data => {
-        courseData = data;
         buildHoles();
+        buildPar(data);
     }
 );
