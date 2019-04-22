8ced82d24ad2e30e82a5fc2d9f62e679e93d854d
clyde9 <samueljclyde@gmail.com>
2019-04-22 15:11:01 -0600

    Added bootstrap, switched to tables

--- main.js
+++ main.js
@@ -51,31 +51,31 @@ class Player{
 
 
 function buildHoles() {
-    $(".container").append(`<div class="row holesRow"><div class="col">Holes</div></div>`);
+    $(".mainTable thead").append(`<tr class="holesRow"><th scope="col"></th></tr>`);
     for (let i = 0; i < 9; i++) {
-        $('.holesRow').append(`<div class="col">${i + 1}</div>`);
+        $('.holesRow').append(`<th scope="col">${i + 1}</th>`);
     }
-    $(".holesRow").append(`<div class="col">Out</div>`);
+    $(".holesRow").append(`<th scope="col">Out</th>`);
     for (let i = 9; i < 18; i++) {
-        $('.holesRow').append(`<div class="col">${i + 1}</div>`);
+        $('.holesRow').append(`<th scope="col">${i + 1}</th>`);
     }
-    $(".holesRow").append(`<div class="col">In</div><div class="col">Total</div>`);
+    $(".holesRow").append(`<th scope="col">In</th><th scope="col">Total</th>`);
 }
 
 function buildPar(courseData){
-    $(".container").append(`<div class="row parRow"><div class="col">Par</div></div>`);
+    $(".mainTable tbody").append(`<tr class="parRow"><th scope="row">Par</th></tr>`);
     let outPar = 0;
     for (let j = 0; j < 9; j++) {
-        $(`.parRow`).append(`<div class="col">${courseData.data.holes[j].teeBoxes[0].par}</div>`);
+        $(`.parRow`).append(`<td>${courseData.data.holes[j].teeBoxes[0].par}</td>`);
         outPar += courseData.data.holes[j].teeBoxes[0].par;
     }
-    $(`.parRow`).append(`<div class="col">${outPar}</div>`);
+    $(`.parRow`).append(`<td>${outPar}</td>`);
     let inPar = 0;
     for (let j = 9; j < 18; j++) {
-        $(`.parRow`).append(`<div class="col">${courseData.data.holes[j].teeBoxes[0].par}</div>`);
+        $(`.parRow`).append(`<td>${courseData.data.holes[j].teeBoxes[0].par}</td>`);
         inPar += courseData.data.holes[j].teeBoxes[0].par;
     }
-    $(`.parRow`).append(`<div class="col">${inPar}</div><div class="col">${outPar + inPar}</div>`);
+    $(`.parRow`).append(`<td>${inPar}</td><td>${outPar + inPar}</td>`);
 }
 
 
